2bb3346cf9b15bd494f6db73971e0ba257bcb24e
Michael Oborne <mich146@hotmail.com>
2020-06-15 16:59:53 +0800

    Update build-in-docker.sh

--- Jellyfin.SkiaSharp.NativeAssets.LinuxMusl/build-in-docker.sh
+++ Jellyfin.SkiaSharp.NativeAssets.LinuxMusl/build-in-docker.sh
@@ -2,4 +2,4 @@
 
 docker build -t libskiasharp-musllinux .
 mkdir -p ./runtimes/linux-musl-x64/native
-docker run --rm -v "./runtimes/linux-musl-x64/native:/temp" "libskiasharp-musllinux" cp /build/skia/out/linux-musl-x64/libSkiaSharp.so /temp/
+docker run --rm -v $(pwd)/runtimes/linux-musl-x64/native:/temp libskiasharp-musllinux cp /build/skia/out/linux-musl-x64/libSkiaSharp.so /temp/
